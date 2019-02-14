4f16d85b9975440a6ae4629c1af8780488aba1d5
benborla <benborla@icloud.com>
2019-02-14 15:47:09 +0800

    updated installer for demo cms

--- etc/MelisDemoCms/config/setup/download.config.php
+++ etc/MelisDemoCms/config/setup/download.config.php
@@ -1,7 +1,180 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: melis-admin
- * Date: 2/14/2019
- * Time: 1:50 PM
- */
+
+namespace MelisDemoCms;
+
+use MelisMarketPlace\Support\MelisMarketPlace as MarketPlace;
+use MelisMarketPlace\Support\MelisMarketPlaceCmsTables as Melis;
+use MelisMarketPlace\Support\MelisMarketPlaceSiteInstall as Site;
+
+return [
+    'plugins' => [
+        __NAMESPACE__ => [
+            Site::DOWNLOAD => [
+                Site::CONFIG => [
+                    'id' => 'id_' . __NAMESPACE__,
+                    Melis::CMS_TOTAL_PAGE => 10,
+                ],
+                MarketPlace::FORM => [
+                    'melis_demo_cms_setup' => [
+                        'forms' => [
+                            'melis_demo_cms_setup_download_form' => [
+                                'attributes' => [
+                                    'name' => 'melis_demo_cms_setup_download_form',
+                                    'id' => 'melis_demo_cms_setup_download_form',
+                                    'method' => 'POST',
+                                    'action' => '',
+                                ],
+                                'hydrator' => \Zend\Stdlib\Hydrator\ArraySerializable::class,
+                                'elements' => [
+                                    [
+                                        'spec' => [
+                                            'name' => 'scheme',
+                                            'type' => \Zend\Form\Element\Select::class,
+                                            'options' => [
+                                                'label' => 'tr_site_demo_cms_tool_site_scheme',
+                                                'tooltip' => 'tr_site_demo_cms_tool_site_scheme tooltip',
+                                                'value_options' => [
+                                                    'http' => 'http://',
+                                                    'https' => 'https://',
+                                                ],
+                                            ],
+                                            'attributes' => [
+                                                'id' => 'scheme',
+                                                'value' => '',
+                                                'required' => 'required',
+                                                'text-required' => '*',
+                                                'class' => 'form-control',
+
+                                            ],
+                                        ],
+                                    ],
+                                    [
+                                        'spec' => [
+                                            'name' => 'domain',
+                                            'type' => \Zend\Form\Element\Text::class,
+                                            'options' => [
+                                                'label' => 'tr_site_demo_cms_tool_site_domain',
+                                                'tooltip' => 'tr_site_demo_cms_tool_site_domain tooltip',
+                                            ],
+                                            'attributes' => [
+                                                'id' => 'domain',
+                                                'value' => '',
+                                                'required' => 'required',
+                                                'placeholder' => 'www.example.com',
+                                                'class' => 'form-control',
+                                                'text-required' => '*',
+                                            ],
+                                        ],
+                                    ],
+                                    [
+                                        'spec' => [
+                                            'name' => 'name',
+                                            'type' => \Zend\Form\Element\Text::class,
+                                            'options' => [
+                                                'label' => 'tr_site_demo_cms_name',
+                                                'tooltip' => 'tr_site_demo_cms_name_tooltip',
+                                            ],
+                                            'attributes' => [
+                                                'id' => 'name',
+                                                'value' => '',
+                                                'required' => 'required',
+                                                'placeholder' => 'My Site Name',
+                                                'class' => 'form-control',
+                                                'text-required' => '*',
+                                            ],
+                                        ],
+                                    ],
+                                ], // end elements
+                                'input_filter' => [
+                                    'scheme' => [
+                                        'name' => 'scheme',
+                                        'required' => true,
+                                        'validators' => [
+                                            [
+                                                'name' => 'InArray',
+                                                'options' => [
+                                                    'haystack' => ['http', 'https'],
+                                                    'messages' => [
+                                                        \Zend\Validator\InArray::NOT_IN_ARRAY => 'tr_site_demo_cms_tool_site_scheme_invalid_selection',
+                                                    ],
+                                                ],
+                                            ],
+                                            [
+                                                'name' => 'NotEmpty',
+                                                'options' => [
+                                                    'messages' => [
+                                                        \Zend\Validator\NotEmpty::IS_EMPTY => 'tr_melis_installer_tool_site_scheme_error_empty',
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                        'filters' => [
+                                        ],
+                                    ],
+                                    'domain' => [
+                                        'name' => 'domain',
+                                        'required' => true,
+                                        'validators' => [
+                                            [
+                                                'name' => 'StringLength',
+                                                'options' => [
+                                                    'encoding' => 'UTF-8',
+                                                    'max' => 50,
+                                                    'messages' => [
+                                                        \Zend\Validator\StringLength::TOO_LONG => 'tr_melis_installer_tool_site_domain_error_long',
+                                                    ],
+                                                ],
+                                            ],
+                                            [
+                                                'name' => 'NotEmpty',
+                                                'options' => [
+                                                    'messages' => [
+                                                        \Zend\Validator\NotEmpty::IS_EMPTY => 'tr_melis_installer_tool_site_domain_error_empty',
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                        'filters' => [
+                                            ['name' => 'StripTags'],
+                                            ['name' => 'StringTrim'],
+                                        ],
+                                    ],
+                                    'name' => [
+                                        'name' => 'name',
+                                        'required' => true,
+                                        'validators' => [
+                                            [
+                                                'name' => 'StringLength',
+                                                'options' => [
+                                                    'encoding' => 'UTF-8',
+                                                    'max' => 50,
+                                                    'messages' => [
+                                                        \Zend\Validator\StringLength::TOO_LONG => 'tr_site_demo_cms_tool_site_name_error_long',
+                                                    ],
+                                                ],
+                                            ],
+                                            [
+                                                'name' => 'NotEmpty',
+                                                'options' => [
+                                                    'messages' => [
+                                                        \Zend\Validator\NotEmpty::IS_EMPTY => 'tr_melis_installer_tool_site_name_error_empty',
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                        'filters' => [
+                                            ['name' => 'StripTags'],
+                                            ['name' => 'StringTrim'],
+                                        ],
+                                    ],
+                                ], // end input_filter
+                            ],
+                        ],
+                    ],
+                ],
+                Site::Data => []
+            ],
+        ],
+    ],
+
+];
--- etc/MelisDemoCms/src/MelisDemoCms/Controller/MelisSetupPostDownloadController.php
+++ etc/MelisDemoCms/src/MelisDemoCms/Controller/MelisSetupPostDownloadController.php
@@ -9,15 +9,11 @@
 
 namespace MelisDemoCms\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-use Zend\View\Model\JsonModel;
-use Zend\Validator\File\Size;
-use Zend\Validator\File\IsImage;
-use Zend\Validator\File\Upload;
-use Zend\File\Transfer\Adapter\Http;
-use Zend\Session\Container;
 use MelisCore\MelisSetupInterface;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Session\Container;
+use Zend\View\Model\JsonModel;
+use Zend\View\Model\ViewModel;
 
 class MelisSetupPostDownloadController extends AbstractActionController implements MelisSetupInterface
 {
@@ -27,12 +23,13 @@ class MelisSetupPostDownloadController extends AbstractActionController implemen
     public function getFormAction()
     {
 
-        $form 		= $this->getFormSiteDemo();
-        $container  = new Container('melis_modules_configuration_status');
-        $formData 	= isset($container['formData']) ? (array) $container['formData'] : null;
+        $form = $this->getFormSiteDemo();
+        $container = new Container('melis_modules_configuration_status');
+        $formData = isset($container['formData']) ? (array) $container['formData'] : null;
 
-        if($formData)
+        if ($formData) {
             $form->setData($formData);
+        }
 
         # Set site label
         $form->get('site_label')->setValue('Melis Demo Cms Site');
@@ -40,8 +37,42 @@ class MelisSetupPostDownloadController extends AbstractActionController implemen
         $view->setVariable('siteDemoCmsForm', $form);
 
         $view->setTerminal(true);
+
         //$view->btnStatus = $btnStatus;
         return $view;
+
+    }
+
+    /**
+     * Create a form from the configuration
+     *
+     * @param $formConfig
+     *
+     * @return \Zend\Form\ElementInterface
+     */
+    private function getFormSiteDemo()
+    {
+        $melisMelisCoreConfig = $this->serviceLocator->get('MelisCoreConfig');
+        $appConfigForm = $melisMelisCoreConfig->getItem('MelisDemoCms/download/form/melis_demo_cms_setup/forms/melis_demo_cms_setup_download_form');
+
+
+        $factory = new \Zend\Form\Factory();
+        $formElements = $this->getServiceLocator()->get('FormElementManager');
+        $factory->setFormElementManager($formElements);
+        $form = $factory->createForm($appConfigForm);
+
+        // default data
+        $scheme = 'https';
+        $domain = $this->getRequest()->getUri()->getHost();
+
+        $data = [
+            'sdom_scheme' => $scheme,
+            'sdom_domain' => $domain,
+        ];
+
+        $form->setData($data);
+
+        return $form;
 
     }
 
@@ -52,123 +83,28 @@ class MelisSetupPostDownloadController extends AbstractActionController implemen
     {
         $success = 0;
         $message = 'tr_install_setup_message_ko';
-        $errors  = array();
+        $errors = [];
 
         $data = $this->getTool()->sanitizeRecursive($this->params()->fromRoute());
 
         $siteDemoCmsForm = $this->getFormSiteDemo();
         $siteDemoCmsForm->setData($data);
 
-        if($siteDemoCmsForm->isValid()) {
+        if ($siteDemoCmsForm->isValid()) {
             $success = 1;
             $message = 'tr_install_setup_message_ok';
-        }
-        else {
+        } else {
             $errors = $this->formatErrorMessage($siteDemoCmsForm->getMessages());
         }
 
 
-        $response = array(
+        $response = [
             'success' => $success,
             'message' => $this->getTool()->getTranslation($message),
-            'errors'  => $errors,
-            'siteDemoCmsForm'    => 'melis_installer_demo_cms',
-            'domainForm'    => 'melis_installer_domain'
-        );
-
-        return new JsonModel($response);
-    }
-
-    /**
-     * @return \Zend\View\Model\JsonModel
-     */
-    public function submitAction()
-    {
-        $success = 0;
-        $message = 'tr_install_setup_message_ko';
-        $errors  = array();
-
-        $data = null;
-        if(!$data)
-            $data = $this->getTool()->sanitizeRecursive($this->params()->fromRoute());
-        // $data = $this->getTool()->sanitizeRecursive($this->params()->fromQuery());
-
-        // Getting the DemoSite config
-        $config = $this->getServiceLocator()->get('config');
-        $siteId = $config['site']['MelisDemoCms']['datas']['site_id'];
-
-        $docPath = $_SERVER['DOCUMENT_ROOT'];
-
-        $setupDatas = include $docPath . '/../module/MelisSites/MelisDemoCms/install/MelisDemoCms.setup.php';
-        $siteData   = $setupDatas['melis_site'];
-
-        $siteDemoCmsForm = $this->getFormSiteDemo();
-        $siteDemoCmsForm->setData($data);
-
-        //Services
-        // $tablePlatformIds = $this->getServiceLocator()->get('MelisEngineTablePlatformIds');
-
-        $container = new \Zend\Session\Container('melis_modules_configuration_status');
-        $hasErrors = false;
-
-
-        if($siteDemoCmsForm->isValid()) {
-
-            try
-            {
-                foreach($container->getArrayCopy() as $module) {
-                    if(!$module)
-                        $hasErrors = true;
-                }
-
-                $container = new \Zend\Session\Container('melismodules');
-                $installerModuleConfigurationSuccess = isset($container['module_configuration']['success']) ?
-                    (bool) $container['module_configuration']['success'] : false;
-
-
-                //siteDemoCms installation start
-                $scheme  = $siteDemoCmsForm->get('sdom_scheme')->getValue();
-                $domain  = $siteDemoCmsForm->get('sdom_domain')->getValue();
-                $siteLabel  = $siteDemoCmsForm->get('site_label')->getValue();
-
-
-                //Save siteDemoCms config
-                if(false === $hasErrors) {
-
-                    /*
-                    * For auto save data
-                    */
-                    // DemoCms Service that process the DemoCms pre-defined datas
-
-
-                    $setupSrv = $this->getServiceLocator()->get('SetupDemoCmsService');
-
-                    // $setupSrv->setupSite($siteData);
-                    $setupSrv->setup(getenv('MELIS_PLATFORM'),$siteLabel);
-                    //$setupSrv->setupSiteDomain($scheme, $domain);
-
-                    $success = 1;
-                    $message = 'tr_install_setup_message_ok';
-                }
-            }
-            catch(\Exception $e)
-            {
-                $errors = $e->getMessage();
-            }
-
-        }
-        else {
-            $errors = $this->formatErrorMessage($siteDemoCmsForm->getMessages());
-        }
-
-
-        $response = array(
-            'success' => $success,
-            'message' => $this->getTool()->getTranslation($message),
-            'errors'  => $errors,
-            'siteDemoCmsForm'    => 'melis_installer_demo_cms',
-            'domainForm'    => 'melis_installer_domain'
-        );
+            'errors' => $errors,
+            'siteDemoCmsForm' => 'melis_installer_demo_cms',
+            'domainForm' => 'melis_installer_domain',
+        ];
 
         return new JsonModel($response);
     }
@@ -185,57 +121,118 @@ class MelisSetupPostDownloadController extends AbstractActionController implemen
         return $melisTool;
 
     }
-    /**
-     * Create a form from the configuration
-     * @param $formConfig
-     * @return \Zend\Form\ElementInterface
-     */
-    private function getFormSiteDemo()
-    {
-        $melisMelisCoreConfig = $this->serviceLocator->get('MelisCoreConfig');
-        $appConfigForm = $melisMelisCoreConfig->getItem('melis_demo_cms_setup/forms/melis_installer_demo_cms');
 
-
-        $factory = new \Zend\Form\Factory();
-        $formElements = $this->getServiceLocator()->get('FormElementManager');
-        $factory->setFormElementManager($formElements);
-        $form = $factory->createForm($appConfigForm);
-
-        // default data
-        $scheme = 'https';
-        $domain = $this->getRequest()->getUri()->getHost();
-
-        $data = array(
-            'sdom_scheme' => $scheme,
-            'sdom_domain' => $domain
-        );
-
-        $form->setData($data);
-
-        return $form;
-
-    }
-
-    private function formatErrorMessage($errors = array())
+    private function formatErrorMessage($errors = [])
     {
 
         $melisMelisCoreConfig = $this->serviceLocator->get('MelisCoreConfig');
 
-        $appConfigForm = $melisMelisCoreConfig->getItem('melis_demo_cms_setup/forms/melis_installer_demo_cms');
+        $appConfigForm = $melisMelisCoreConfig->getItem('MelisDemoCms/download/form/melis_demo_cms_setup/forms/melis_demo_cms_setup_download_form');
 
         $appConfigForm = $appConfigForm['elements'];
 
-        foreach ($errors as $keyError => $valueError)
-        {
-            foreach ($appConfigForm as $keyForm => $valueForm)
-            {
+        foreach ($errors as $keyError => $valueError) {
+            foreach ($appConfigForm as $keyForm => $valueForm) {
                 if ($valueForm['spec']['name'] == $keyError &&
-                    !empty($valueForm['spec']['options']['label']))
+                    !empty($valueForm['spec']['options']['label'])) {
                     $errors[$keyError]['label'] = $valueForm['spec']['options']['label'];
+                }
             }
         }
 
 
         return $errors;
+    }
+
+    /**
+     * @return \Zend\View\Model\JsonModel
+     */
+    public function submitAction()
+    {
+        $success = 0;
+        $message = 'tr_install_setup_message_ko';
+        $errors = [];
+
+        $data = null;
+        if (!$data) {
+            $data = $this->getTool()->sanitizeRecursive($this->params()->fromRoute());
+        }
+        // $data = $this->getTool()->sanitizeRecursive($this->params()->fromQuery());
+
+        // Getting the DemoSite config
+        $config = $this->getServiceLocator()->get('config');
+        $siteId = $config['site']['MelisDemoCms']['datas']['site_id'];
+
+        $docPath = $_SERVER['DOCUMENT_ROOT'];
+
+        $setupDatas = include $docPath . '/../module/MelisSites/MelisDemoCms/install/MelisDemoCms.setup.php';
+        $siteData = $setupDatas['melis_site'];
+
+        $siteDemoCmsForm = $this->getFormSiteDemo();
+        $siteDemoCmsForm->setData($data);
+
+        //Services
+        // $tablePlatformIds = $this->getServiceLocator()->get('MelisEngineTablePlatformIds');
+
+        $container = new \Zend\Session\Container('melis_modules_configuration_status');
+        $hasErrors = false;
+
+
+        if ($siteDemoCmsForm->isValid()) {
+
+            try {
+                foreach ($container->getArrayCopy() as $module) {
+                    if (!$module) {
+                        $hasErrors = true;
+                    }
+                }
+
+                $container = new \Zend\Session\Container('melismodules');
+                $installerModuleConfigurationSuccess = isset($container['module_configuration']['success']) ?
+                    (bool) $container['module_configuration']['success'] : false;
+
+
+                //siteDemoCms installation start
+                $scheme = $siteDemoCmsForm->get('sdom_scheme')->getValue();
+                $domain = $siteDemoCmsForm->get('sdom_domain')->getValue();
+                $siteLabel = $siteDemoCmsForm->get('site_label')->getValue();
+
+
+                //Save siteDemoCms config
+                if (false === $hasErrors) {
+
+                    /*
+                    * For auto save data
+                    */
+                    // DemoCms Service that process the DemoCms pre-defined datas
+
+
+                    $setupSrv = $this->getServiceLocator()->get('SetupDemoCmsService');
+
+                    // $setupSrv->setupSite($siteData);
+                    $setupSrv->setup(getenv('MELIS_PLATFORM'), $siteLabel);
+                    //$setupSrv->setupSiteDomain($scheme, $domain);
+
+                    $success = 1;
+                    $message = 'tr_install_setup_message_ok';
+                }
+            } catch (\Exception $e) {
+                $errors = $e->getMessage();
+            }
+
+        } else {
+            $errors = $this->formatErrorMessage($siteDemoCmsForm->getMessages());
+        }
+
+
+        $response = [
+            'success' => $success,
+            'message' => $this->getTool()->getTranslation($message),
+            'errors' => $errors,
+            'siteDemoCmsForm' => 'melis_installer_demo_cms',
+            'domainForm' => 'melis_installer_domain',
+        ];
+
+        return new JsonModel($response);
     }
 }
