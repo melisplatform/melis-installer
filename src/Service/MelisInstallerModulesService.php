8a1d805f3d02c8906d34c66d846c1503c2e3a946
Ralph Bryan Brioso <bbrioso@melistechnology.com>
2020-05-20 12:01:46 +0800

    Getting melis modules from vendor

--- src/Service/MelisInstallerModulesService.php
+++ src/Service/MelisInstallerModulesService.php
@@ -70,10 +70,16 @@ class MelisInstallerModulesService implements ServiceLocatorAwareInterface
 
             $type = $package->type;
             $extra = $package->extra ?? [];
+            $isMelisModule = true;
+            if (array_key_exists('melis-module', $extra)) {
+                $key = 'melis-module';
+                if (!$extra->$key)
+                    $isMelisModule = false;
+            }
 
             /** @var CompletePackage $package */
             return $type === 'melisplatform-module' &&
-                array_key_exists('module-name', $extra);
+                array_key_exists('module-name', $extra) && $isMelisModule;
         });
 
         $modules = array_map(function ($package) {
