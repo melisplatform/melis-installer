440baa23b11e79ddab4d9bffd09290f0aee14c79
unknown <crismakabenta@gmail.com>
2020-05-29 16:53:46 +0800

    Update getting modules in displaying form

--- src/Controller/InstallerController.php
+++ src/Controller/InstallerController.php
@@ -1158,6 +1158,14 @@ class InstallerController extends AbstractActionController
             }
 
             $downloadableModules = array_merge($autoInstallModules, $downloadableModules);
+
+            if(array_key_exists('MelisEngine', $downloadableModules)) unset($downloadableModules['MelisEngine']);
+            if(array_key_exists('MelisFront', $downloadableModules)) unset($downloadableModules['MelisFront']);
+            if(array_key_exists('MelisCms', $downloadableModules)) unset($downloadableModules['MelisCms']);
+            if(array_key_exists('MelisCmsNews', $downloadableModules)) unset($downloadableModules['MelisCmsNews']);
+            if(array_key_exists('MelisCmsProspects', $downloadableModules)) unset($downloadableModules['MelisCmsProspects']);
+            if(array_key_exists('MelisCmsSlider', $downloadableModules)) unset($downloadableModules['MelisCmsSlider']);
+
             $downloadableModules = implode(' ', $downloadableModules);
 
             $composerSvc = $this->getServiceManager()->get('MelisComposerService');
@@ -1166,7 +1174,7 @@ class InstallerController extends AbstractActionController
             ini_set('memory_limit', -1);
 
             if (!$this->isUsingCoreOnly()) {
-                $composerSvc->download($downloadableModules, null, true);
+                $composerSvc->download($downloadableModules, "dev-migrate/laminas", true);
             } else {
                 $autoInstallModules = implode(' ', $autoInstallModules);
                 $composerSvc->download($autoInstallModules, null, true);
@@ -1631,8 +1639,8 @@ class InstallerController extends AbstractActionController
         set_time_limit(0);
         ini_set('memory_limit', -1);
 
-        $mm = $this->getServiceManager()->get('ModuleManager');
-        $modules = array_keys($mm->getLoadedModules());
+        $moduleService = $this->getServiceManager()->get('MelisInstallerModulesService');
+        $modules = $moduleService->getAllModules();
         $modules = array_diff($modules, $this->getInstallerModules());
         $modulesExclusions = [
             'MelisEngine',
